ff4cec005cbf0e4cc808d886fb69f50d5c5b4b9c
Jeffrey Allen <jeffwayne256@gmail.com>
2025-08-23 08:31:54 -0500

    Basic server with mux

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"io"
+	"net/http"
+	"time"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/hello", hello)
+
+	s := &http.Server{
+		Addr:         ":8080",
+		Handler:      mux,
+		ReadTimeout:  2 * time.Second,
+		WriteTimeout: 2 * time.Second,
+		IdleTimeout:  5 * time.Second,
+	}
+
+	s.ListenAndServe()
+}
+
+func hello(w http.ResponseWriter, req *http.Request) {
+	// Test breaking the connection
+	// time.Sleep(3 * time.Second)
+	io.WriteString(w, "Hello World!")
+}
